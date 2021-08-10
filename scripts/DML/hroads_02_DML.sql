a6e88d6268cc0fd2b76829290e64cc6209ea8b82
ApolloSama <pedro.intropedi@gmail.com>
2021-08-10 08:54:03 -0300

    DDL e DML concluídos

---
+++ scripts/DML/hroads_02_DML.sql
@@ -0,0 +1,32 @@
+﻿USE SENAI_HROADS_TARDE;
+GO
+
+INSERT INTO tipoHabilidade (nomeTipo)
+VALUES ('Ataque'),('Defesa'),('Cura'),('Magia');
+GO
+
+INSERT INTO habilidade (idTipo, nomeHabilidade)
+VALUES (1,'Lança Mortal'),(2,'Escudo Supremo'),(3,'Recuperar Vida');
+GO
+
+INSERT INTO classe (nomeClasse)
+VALUES ('Bárbaro'),('Cruzado'),('Caçadora de Demônios'),('Monge'),('Necromante'),('Feiticeiro'),('Arcanista');
+GO
+
+SELECT * FROM classe;
+
+UPDATE classe set nomeClasse = 'Necromancer'
+WHERE idClasse = 7
+
+INSERT INTO classe_habilidade (idClasse, idHabilidade)
+VALUES (1,1),(1,2),(2,2),(3,1),(4,3),(4,2),(5,NULL),(6,3),(7,NULL);
+GO
+
+INSERT INTO personagem (idClasse, nomePersonagem, vidaMax, manaMax, dataCriacao, dataAtualizacao)
+VALUES (1,'DeuBug',100,80,'18/01/2019','10/08/2021'),(4,'BitBug',70,100,'17/03/2016','10/08/2021'),
+(7,'Fer8',75,60,'18/03/2018','10/08/2021');
+
+DELETE FROM personagem;
+
+UPDATE personagem set nomePersonagem = 'Fer7'
+WHERE idPersonagem = 6
---
+++ scripts/DQL/hroads_03_DQL.sql
@@ -0,0 +1,7 @@
+USE SENAI_HROADS_TARDE;
+
+SELECT * FROM tipoHabilidade;
+SELECT * FROM habilidade;
+SELECT * FROM classe_habilidade;
+SELECT * FROM classe;
+SELECT * FROM personagem;
